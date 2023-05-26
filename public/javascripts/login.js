b702c1cb426e8047679d6431fc0fe5aadb811bee
cux-maks <volcano020716@gmail.com>
2023-05-26 22:10:57 +0900

    reject 제거, main화면에 테스트 버튼 제거

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -6,12 +6,29 @@ loginButton.addEventListener("click", () => {
     alert("id, pw를 입력해주세요");
   }
 
+    /*
+    fetch에서 매개변수 사용하려면?
+
+    params는 URLSearchParams라는 새 변수를 만들어서 이렇게 선언해주면 된다.
+    파이썬 딕셔너리처럼! 사용!
+    C++에서는 아마 해시맵인가? 그럴거임
+
+    const params = new URLSearchParams({
+    limit: 10,
+    sort: 'asc'
+    });
+
+    const url = `/users?${params}`;
+
+    이렇게 '/users'와 같은 엔드포인트 뒤에 '?${params}' 추가!    
+    */
+
   const params = new URLSearchParams({
     id: idValue,
     pw: pwValue
   })
 
-  fetch("/users?${params}")
+  fetch(`/users?${params}`)
   .then(response => response.json())
   .then(data => {
    // 받아온 사용자 목록을 처리하는 코드 작성
