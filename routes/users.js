258853b9f139769661d80b9dc55dde87c30be400
MinGu Jeong <mingu830@gmail.com>
2023-06-01 09:35:00 +0900

    Revert "Merge pull request #16 from MinGu-Jeong/feature/search"
    This reverts commit 603137e36c315bdfc016b1433ab334cf5b5fb82f, reversing
    changes made to f530c315d46769594c96c67b4b06643758d42969.

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql');
-
-const con = mysql.createConnection({
-  host:'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'MyCocktail'
-})
-
-con.connect(function(err){
-  if(err) throw err;
-  console.log('Connected');
-})
-
-// 엔드포인트 설정
-// 아마 fetch에서 배개변수 넘겨주면 req에 들어가는듯?
-// -> "req.query.변수이름"으로 접근 가능 -> 파이썬의 딕셔너리처럼 인덱스 번호 접근이 아니라 변수 이름으로!!
-// C++에서는 아마 해시맵? 일듯?
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-
-  con.query("SELECT * FROM member", (err, result) =>{
-    if(err){
-      reject(err);
-      return;
-    }
-    res.json(result)
-  })
-})
-
-router.post('/', (req, res) => {
-  
-})
-
-router.put('/', (req, res) => {
-
-})
-
-router.delete('/', (req, res) => {
-  
-})
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
