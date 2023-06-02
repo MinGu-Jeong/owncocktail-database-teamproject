b4203b1371f6eae20185f81eb402256efe973244
MinGu Jeong <mingu830@gmail.com>
2023-06-02 20:27:39 +0900

    로그인 성공시 메인페이지로 이동

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -69,17 +69,17 @@ loginButton.addEventListener("click", (e) => {
       // 검색 후 받아온 데이터 보는거
       // console.log(data[0]);
       console.log(data);
-      alert(data.message);
 
       // 로그인 성공시 세션스토리지에 사용자 정보 저장
       if (data.message === "로그인 성공") {
         sessionStorage.setItem("user", JSON.stringify(data.user));
-        console.log("로그인 성공");
+        window.location.href = "./index.html";
       } else {
-        console.log("로그인 실패");
+        alert("아이디 또는 비밀번호가 일치하지 않습니다.");
       }
     })
     .catch((error) => {
+      alert("아이디 또는 비밀번호가 일치하지 않습니다.");
       console.error(
         "There has been a problem with your fetch operation:",
         error
