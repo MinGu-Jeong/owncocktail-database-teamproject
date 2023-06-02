e19f6aee7c92822f5ecd317bd3b922fd24d2de8d
JeongHoon <cjh41820@gmail.com>
2023-06-02 21:30:29 +0900

    재료, 검색 html,css 수정 재료 js수정

--- public/javascripts/ingredient.js
+++ public/javascripts/ingredient.js
@@ -20,3 +20,27 @@ function changeSort(sortType) {
   var dropdownContent = document.getElementById("dropdown-content");
   dropdownContent.classList.remove("active");
 }
+
+const $loginButton = document.getElementById("login-button");
+const $signupButton = document.getElementById("signup-button");
+const $receipeButton = document.getElementById("nav-cocktail-receipe");
+const $ingredientButton = document.getElementById("nav-ingredient");
+const $searchButton = document.getElementById("nav-search");
+const $titleLogo = document.querySelector(".title-logo");
+$titleLogo.addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
+$loginButton.addEventListener("click", () => {
+  window.location.href = "./login.html";
+});
+
+$signupButton.addEventListener("click", () => {
+  window.location.href = "./signup.html";
+});
+
+$ingredientButton.addEventListener("click", () => {
+  window.location.href = "./ingredient.html";
+});
+$searchButton.addEventListener("click", () => {
+  window.location.href = "./search.html";
+});
