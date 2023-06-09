baa41ff8bdfe6b337967da73099b9802cacfbc11
cux-maks <volcano020716@gmail.com>
2023-06-09 19:51:59 +0900

    api 작성 완

--- routes/write.js
+++ routes/write.js
@@ -14,16 +14,6 @@ con.connect(function(err){
   console.log('Connected');
 })
 
-// 엔드포인트 설정
-// 아마 fetch에서 배개변수 넘겨주면 req에 들어가는듯?
-// -> "req.query.변수이름"으로 접근 가능 -> 파이썬의 딕셔너리처럼 인덱스 번호 접근이 아니라 변수 이름으로!!
-// C++에서는 아마 해시맵? 일듯?
- 
-/* GET users listing. */
-router.get('/', (req, res) => {
-    
-})
-
 router.post('/default_board', (req, res) => {
 
 	if(req.body.member_id !== 'admin'){
