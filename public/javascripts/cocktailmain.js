f00394b71e73ede2751ed9f73b2ac9f85e46e172
hoooooony <cjh41820@gmail.com>
2023-06-10 23:28:10 +0900

    칵테일 메인 js 수정

--- public/javascripts/cocktailmain.js
+++ public/javascripts/cocktailmain.js
@@ -47,7 +47,6 @@ const $cocktailName5 = document.querySelector("#cocktail-name1");
 const $cocktailName6 = document.querySelector("#cocktail-name2");
 const $cocktailName7 = document.querySelector("#cocktail-name3");
 const $cocktailName8 = document.querySelector("#cocktail-name4");
-
 const $cocktailId1 = document.querySelector("#cocktail-id1");
 const $cocktailId2 = document.querySelector("#cocktail-id2");
 const $cocktailId3 = document.querySelector("#cocktail-id3");
@@ -56,7 +55,6 @@ const $cocktailId5 = document.querySelector("#cocktail-id1");
 const $cocktailId6 = document.querySelector("#cocktail-id2");
 const $cocktailId7 = document.querySelector("#cocktail-id3");
 const $cocktailId8 = document.querySelector("#cocktail-id4");
-
 
 window.onload = function () {
   const $loginButtonTop = document.querySelector("#login-button");
@@ -127,38 +125,43 @@ window.onload = function () {
       console.error;
     });
 
-    fetch("/search/popular_default_board", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        page: 1*(pageCount),
-        num: 8*(pageCount),
-      }),
+  fetch("/search/popular_default_board", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      page: 1 * pageCount,
+      num: 8 * pageCount,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < 8; i++) {
+        document.querySelector(
+          `#best-cocktail-${i + 1} .cocktail-name`
+        ).textContent = data[i].recipe_name;
+        document.querySelector(
+          `#best-cocktail-${i + 1} .cocktail-summary`
+        ).textContent = data[i].board_id; // '설명' 대신 'board_id'를 표시합니다.
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        for (let i = 0; i < 8; i++) {
-          document.querySelector(`#best-cocktail-${i+1} .cocktail-name`).textContent = data[i].recipe_name;
-          document.querySelector(`#best-cocktail-${i+1} .cocktail-summary`).textContent = data[i].board_id; // '설명' 대신 'board_id'를 표시합니다.
-        }
-      })
-      .catch((error) => {
-        console.error;
-      });
-  
-      const $bestCocktailButtonContainer = document.querySelector(".best-cocktail-button-container");
+    .catch((error) => {
+      console.error;
+    });
 
-      $bestCocktailButtonContainer.addEventListener("click", (event) => {
-        const cocktailButton = event.target.closest("button");
-        if (!cocktailButton) return;
-        const boardId = cocktailButton.querySelector(".cocktail-summary").textContent; // 'cocktailId' 대신 'boardId'를 사용합니다.
-        window.location.href = "./cocktail.html?id=" + boardId + "&type=default";
-      });      
+  const $bestCocktailButtonContainer = document.querySelector(
+    ".best-cocktail-button-container"
+  );
 
-
+  $bestCocktailButtonContainer.addEventListener("click", (event) => {
+    const cocktailButton = event.target.closest("button");
+    if (!cocktailButton) return;
+    const boardId =
+      cocktailButton.querySelector(".cocktail-summary").textContent; // 'cocktailId' 대신 'boardId'를 사용합니다.
+    window.location.href = "./cocktail.html?id=" + boardId + "&type=default";
+  });
 };
 function dropdown() {
   var v = document.querySelector(".dropdown-content");
@@ -166,22 +169,6 @@ function dropdown() {
   v.classList.toggle("show");
   dropbtn.style.borderColor = "rgb(94, 94, 94)";
 }
-const $cocktailName1 = document.querySelector("#cocktail-name1");
-const $cocktailName2 = document.querySelector("#cocktail-name2");
-const $cocktailName3 = document.querySelector("#cocktail-name3");
-const $cocktailName4 = document.querySelector("#cocktail-name4");
-const $cocktailName5 = document.querySelector("#cocktail-name5");
-const $cocktailName6 = document.querySelector("#cocktail-name6");
-const $cocktailName7 = document.querySelector("#cocktail-name7");
-const $cocktailName8 = document.querySelector("#cocktail-name8");
-const $cocktailId1 = document.querySelector("#cocktail-id1");
-const $cocktailId2 = document.querySelector("#cocktail-id2");
-const $cocktailId3 = document.querySelector("#cocktail-id3");
-const $cocktailId4 = document.querySelector("#cocktail-id4");
-const $cocktailId5 = document.querySelector("#cocktail-id5");
-const $cocktailId6 = document.querySelector("#cocktail-id6");
-const $cocktailId7 = document.querySelector("#cocktail-id7");
-const $cocktailId8 = document.querySelector("#cocktail-id8");
 function draw(path, thispage) {
   fetch(`/search/${path}_default_board`, {
     method: "POST",
