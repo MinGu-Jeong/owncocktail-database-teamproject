b007d60b81b120205699ffee449d0621211451ff
hoooooony <cjh41820@gmail.com>
2023-06-11 00:07:33 +0900

    재료 디테일 수정사항

---
+++ public/javascripts/ingredientdetail.js
@@ -0,0 +1,103 @@
+var searchParams = new URLSearchParams(window.location.search);
+const cocktailId = searchParams.get("id");
+const cocktailType = searchParams.get("type");
+const $loginButton = document.getElementById("login-button");
+const $signupButton = document.getElementById("signup-button");
+const $receipeButton = document.getElementById("nav-cocktail-receipe");
+const $ingredientButton = document.getElementById("nav-ingredient");
+const $searchButton = document.getElementById("nav-search");
+const $mycocktailmain = document.getElementById("nav-own-cocktail");
+
+const $titleLogo = document.querySelector(".title-logo");
+$titleLogo.addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
+$loginButton.addEventListener("click", () => {
+  window.location.href = "./login.html";
+});
+
+$signupButton.addEventListener("click", () => {
+  window.location.href = "./signup.html";
+});
+
+$ingredientButton.addEventListener("click", () => {
+  window.location.href = "./ingredient.html";
+});
+$searchButton.addEventListener("click", () => {
+  window.location.href = "./search.html";
+});
+$mycocktailmain.addEventListener("click", () => {
+  window.location.href = "./mycocktailmain.html";
+});
+$receipeButton.addEventListener("click", () => {
+  window.location.href = "./cocktailmain.html";
+});
+const $loginButtonTop = document.querySelector("#login-button");
+const $signupButtonTop = document.querySelector("#signup-button");
+const user = JSON.parse(sessionStorage.getItem("user"));
+if (user && user.isLogin) {
+  // 로그인이 된 상태
+  $loginButtonTop.textContent = "로그아웃";
+  $loginButtonTop.onclick = function () {
+    // 로그아웃 로직 실행
+    sessionStorage.removeItem("user"); // 세션스토리지에서 사용자 정보 삭제
+    window.location.reload(); // 페이지 새로고침
+  };
+
+  $signupButtonTop.textContent = "마이페이지";
+  $signupButtonTop.onclick = function () {
+    // 마이페이지로 이동
+    window.location.href = "./mypage.html";
+  };
+} else {
+  // 로그인이 되지 않은 상태
+  $loginButtonTop.onclick = function () {
+    // 로그인 페이지로 이동
+    window.location.href = "./login.html";
+  };
+
+  $signupButtonTop.onclick = function () {
+    // 회원가입 페이지로 이동
+    window.location.href = "./signup.html";
+  };
+}
+window.onload = () => {
+  document.querySelector(".dropbtn_click").onclick = () => {
+    dropdown();
+  };
+  for (let i = 0; i < options.length; i++) {
+    options[i].onclick = () => {
+      showMenu(options[i].innerText);
+    };
+  }
+
+  const $loginButtonTop = document.querySelector("#login-button");
+  const $signupButtonTop = document.querySelector("#signup-button");
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  if (user && user.isLogin) {
+    // 로그인이 된 상태
+    $loginButtonTop.textContent = "로그아웃";
+    $loginButtonTop.onclick = function () {
+      // 로그아웃 로직 실행
+      sessionStorage.removeItem("user"); // 세션스토리지에서 사용자 정보 삭제
+      window.location.reload(); // 페이지 새로고침
+    };
+
+    $signupButtonTop.textContent = "마이페이지";
+    $signupButtonTop.onclick = function () {
+      // 마이페이지로 이동
+      window.location.href = "./mypage.html";
+    };
+  } else {
+    // 로그인이 되지 않은 상태
+    $loginButtonTop.onclick = function () {
+      // 로그인 페이지로 이동
+      window.location.href = "./login.html";
+    };
+
+    $signupButtonTop.onclick = function () {
+      // 회원가입 페이지로 이동
+      window.location.href = "./signup.html";
+    };
+  }
+};
