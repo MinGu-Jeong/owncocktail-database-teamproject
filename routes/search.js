4899c6ddd16777cae1d6bec53be06c7114779f79
cux-maks <volcano020716@gmail.com>
2023-06-12 19:06:35 +0900

    popular_default/my_recipe api 추가

--- routes/search.js
+++ routes/search.js
@@ -115,6 +115,13 @@ router.post("/popular_default_board", (req, res) => {
   );
 });
 
+router.post("/popular_default_recipe", (req, res) => {
+  con.query(`SELECT \`recipe_name\`, \`img_url\` FROM \`Recipe\` WHERE \`recipe_name\` IN (SELECT \`recipe_name\` FROM \`Default_Board\` ORDER BY \`good_cnt\` DESC, \`recipe_name\` ASC LIMIT ${(req.body.page - 1) * req.body.num}, ${req.body.num}`, (err, result) => {
+    if(err) throw err
+    res.json(result);
+  })
+})
+
 router.post("/name_default_board", (req, res) => {
   con.query(
     `SELECT \`recipe_name\`, \`board_id\`, \`member_id\`, \`write_time\`, \`good_cnt\` FROM \`Default_Board\` ORDER BY \`recipe_name\` LIMIT ${
@@ -147,6 +154,13 @@ router.post("/popular_my_board", (req, res) => {
     }
   );
 });
+
+router.post("/popular_my_recipe", (req, res) => {
+  con.query(`SELECT \`recipe_name\`, \`img_url\` FROM \`Recipe\` WHERE \`recipe_name\` IN (SELECT \`recipe_name\` FROM \`My_Board\` ORDER BY \`good_cnt\` DESC, \`recipe_name\` ASC LIMIT ${(req.body.page - 1) * req.body.num}, ${req.body.num}`, (err, result) => {
+    if(err) throw err
+    res.json(result);
+  })
+})
 
 router.post("/name_my_board", (req, res) => {
   con.query(
